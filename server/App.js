934b14cfee12287f7a383b4b31fee92d20217fce
nan <103703190+legn0@users.noreply.github.com>
2023-10-25 20:06:31 -0300

    me faltaron cosas

---
+++ server/App.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+
+//importar base de datos
+const db = require("./Database");
+const app = express();
+
+app.set("port", process.env.PORT || 5000);
+
+//Middlewares
+
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(cors());
+
+
+db();
+
+app.listen(app.get("port"), ()=>{
+    console.log('Servidor esta corriendo en el puerto: ${app.get("port")}');
+})
+
+module.exports = app;
