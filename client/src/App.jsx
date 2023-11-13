623f11c3ff8ab49e43ea8d1a0211edec0a1366ff
Matty <105567132+Matty115@users.noreply.github.com>
2023-11-13 01:41:59 -0300

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,28 +5,15 @@ import MiniaturaBloqueHorario from "./components/MiniaturaBloqueHorario";
 import Calendar from "./components/Calendar";
 
 function App() {
-  //datos de mentira
-  let bloques_usados = ["8:30", "11:00"];
   return (
-    <Calendar />
+    
+    <Routes>
+        <Route exact path='/' element={<MainPage />} />
+        <Route exact path='/calendar' element={<Calendar />} />
+    </Routes>
 
-    // <div className="planilla">
-    //   <ColumnaDia
-    //     className="columna-dia"
-    //     tipoExamen="Radiografia"
-    //     dia="Lunes"
-    //     fecha="05-08-2024"
-    //   />
-    // <ColumnaDia
-    //   className="columna-dia"
-    //   tipoExamen="Radiografia"
-    //   dia="Martes"
-    //   fecha="06-08-2024"
-    // />
-
-    //   <MiniaturaExamen nombrepaciente="Juan" medicotratante="pepe" />
-    // </div>
   );
 }
+
 
 export default App;
